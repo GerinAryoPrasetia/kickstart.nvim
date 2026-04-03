152ad5fb920870ba150ad214a4d0e1a5f4f9d20c
Gerin Aryo <gerinaryo@Gerins-MacBook-Pro-2.local>
2026-04-04 00:05:28 +0700

    update nvim-treesitter branch

--- init.lua
+++ init.lua
@@ -1048,7 +1048,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    branch = 'master',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
